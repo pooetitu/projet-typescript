23f7d1839917a4908f1a841659796956be0afb0e
pooetitu <pooetitu@gmail.com>
2021-04-30 16:04:26 +0200

    Add pokemon properties to make the damage calculation

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -4,13 +4,27 @@ export class Pokemon {
     name: string;
     speed: number;
     health: number;
+    level:number;
+    attack:number;
+    defense:number;
     attacks: Attack[];
 
-    constructor(name: string, speed: number, health: number, attacks: Attack[]) {
+    constructor(name: string, speed: number, health: number, level: number, attack: number, defense: number, attacks: Attack[]) {
         this.name = name;
         this.speed = speed;
         this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defense = defense;
         this.attacks = attacks;
+    }
+
+    public receiveDamage(damage: number){
+        this.health -= damage;
+    }
+
+    public attackPokemon(pokemon: Pokemon, attack: number){
+        this.attacks[attack].attack(this, pokemon);
     }
 }
 
@@ -22,4 +36,13 @@ export function firstPokemonToFight(first: Pokemon, second: Pokemon): Pokemon {
         return first;
     }
     return second;
+}
+
+export function selectRandomAttack(pokemon: Pokemon): Attack | null {
+    const availableAttacks = pokemon.attacks.filter(p => p.pp > 0);
+    if(availableAttacks.length <= 0){
+        return null;
+    }
+    const index = Math.random() * availableAttacks.length;
+    return availableAttacks[index];
 }
--- test/pokemon.test.ts
+++ test/pokemon.test.ts
@@ -1,8 +1,8 @@
 import {firstPokemonToFight, Pokemon} from "../src/models/pokemon";
 
-let salameche = new Pokemon("salameche", 50,60,[]);
-let herbizarre = new Pokemon("herbizarre", 50,40,[]);
-let carapuce = new Pokemon("carapuce", 45,50,[]);
+let salameche = new Pokemon("salameche", 50,60,1,10,10,[]);
+let herbizarre = new Pokemon("herbizarre", 50,40,1,10,10,[]);
+let carapuce = new Pokemon("carapuce", 45,50,1,10,10,[]);
 
 describe('PokemonAttack', () => {
     it('should show the name of the pokemon salameche', () => {
@@ -12,14 +12,17 @@ describe('PokemonAttack', () => {
     it('should return salameche wich has the highest speed', () => {
         expect(firstPokemonToFight(salameche, carapuce)).toBe(salameche);
     });
-
-    beforeEach(() => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0);
-    });
-    it('should return a random pokemon when their speed is equal', () => {
-        expect(firstPokemonToFight(salameche, herbizarre)).toBe(salameche);
-    });
-    afterEach(() => {
-        jest.spyOn(global.Math, 'random').mockRestore();
+    describe("should return a random pokemon when their speed is equal",() =>{
+        it('should return first pokemon when rendom return 0', () => {
+            jest.spyOn(global.Math, 'random').mockReturnValue(0)
+            expect(firstPokemonToFight(salameche, herbizarre)).toBe(salameche);
+        });
+        it('should return second pokemon when rendom return 1', () => {
+            jest.spyOn(global.Math, 'random').mockReturnValue(1)
+            expect(firstPokemonToFight(salameche, herbizarre)).toBe(herbizarre);
+        });
+        afterEach(() => {
+            jest.spyOn(global.Math, 'random').mockRestore();
+        });
     });
 });
