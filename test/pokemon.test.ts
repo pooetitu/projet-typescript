9a0249d39044716041e5f76037c32b8803f0a251
pooetitu <pooetitu@gmail.com>
2021-04-30 16:53:11 +0200

    Add isAlive test

--- test/pokemon.test.ts
+++ test/pokemon.test.ts
@@ -2,7 +2,7 @@ import {firstPokemonToFight, Pokemon} from "../src/models/pokemon";
 
 let salameche = new Pokemon("salameche", 50,60,1,10,10,[]);
 let herbizarre = new Pokemon("herbizarre", 50,40,1,10,10,[]);
-let carapuce = new Pokemon("carapuce", 45,50,1,10,10,[]);
+let carapuce = new Pokemon("carapuce", 45,0,1,10,10,[]);
 
 describe('PokemonAttack', () => {
     it('should show the name of the pokemon salameche', () => {
@@ -24,5 +24,11 @@ describe('PokemonAttack', () => {
         afterEach(() => {
             jest.spyOn(global.Math, 'random').mockRestore();
         });
+    });
+    it('should return true if it has less than 0 health point', () => {
+        expect(carapuce.isDead()).toBe(true);
+    });
+    it('should return false if it has more than 0 health point', () => {
+        expect(carapuce.isDead()).toBe(false);
     });
 });
