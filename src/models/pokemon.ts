31ff3651c96deb74292d019b53e36d11d70fb700
pooetitu <pooetitu@gmail.com>
2021-04-30 16:52:36 +0200

    Add isAlive and fixes attack selection

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -23,12 +23,12 @@ export class Pokemon {
         this.health -= damage;
     }
 
-    public attackPokemon(pokemon: Pokemon, attack: number){
-        this.attacks[attack].attack(this, pokemon);
+    public isDead(): boolean{
+        return this.health <= 0;
     }
 }
 
-export function firstPokemonToFight(first: Pokemon, second: Pokemon): Pokemon {
+export async function firstPokemonToFight(first: Pokemon, second: Pokemon): Promise<Pokemon> {
     if (first.speed === second.speed) {
         return (Math.round(Math.random()) === 0 ? first:second);
     }
@@ -38,11 +38,12 @@ export function firstPokemonToFight(first: Pokemon, second: Pokemon): Pokemon {
     return second;
 }
 
-export function selectRandomAttack(pokemon: Pokemon): Attack | null {
+export async function selectRandomAttack(pokemon: Pokemon): Promise<Attack | null> {
     const availableAttacks = pokemon.attacks.filter(p => p.pp > 0);
-    if(availableAttacks.length <= 0){
+    if(availableAttacks.length <= 0) {
+        console.log(`${pokemon.name} can't attack because he has no pp left`)
         return null;
     }
-    const index = Math.random() * availableAttacks.length;
+    const index = Math.floor(Math.random() * availableAttacks.length);
     return availableAttacks[index];
 }
