b90e56fb8fbdb1ff55d314271aaba6f1c6bab6e5
pooetitu <pooetitu@gmail.com>
2021-04-30 16:53:00 +0200

    Add message on attack

--- src/models/attack.ts
+++ src/models/attack.ts
@@ -6,16 +6,18 @@ export class Attack {
     pp: number;
     ppMax: number;
 
-    constructor(name: string, damage: number, pp: number, ppMax: number) {
+    constructor(name: string, damage: number, ppMax: number) {
         this.name = name;
         this.damage = damage;
-        this.pp = pp;
+        this.pp = ppMax;
         this.ppMax = ppMax;
     }
 
-    public attack(attacker: Pokemon, defending: Pokemon) {
+    public async attack(attacker: Pokemon, defending: Pokemon) {
         if(this.pp >0) {
-            defending.receiveDamage(Math.floor(Math.floor(Math.floor(2 * attacker.level / 5 + 2) * attacker.attack * this.damage / defending.defense) / 50) + 2);
+            const damage = Math.floor(Math.floor(Math.floor(2 * attacker.level / 5 + 2) * attacker.attack * this.damage / defending.defense) / 50) + 2
+            defending.receiveDamage(damage);
+            console.log(`${attacker.name} attacks ${defending.name} with ${this.name}, dealing ${damage} damage`)
             this.pp--;
         }
     }
